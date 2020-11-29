5dfcf443f144c19152abb88f5ab3763d1a4df5b8
alandaleote <alandaleote@gmail.com>
2020-11-29 00:12:44 -0300

    @alinenecchhi iniciando componentes para forms

---
+++ src/Pages/OngHome/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+import Header from'../../componentes/Header'
+
+
+function Ong() {
+  return (
+      <Header/>
+     
+);
+}
+export default Ong;
---
+++ src/componentes/FormOng/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css";
+
+function FormOng() {
+    const msg = "Formulario vai aqui"
+  return (
+
+    msg
+  );
+}
+export default FormOng;
